7f33cd28c98dbfc93ced059d8b1b21796bdf99b8
chenbiao <149027310@qq.com>
2025-01-03 14:14:29 +0800

    init

--- munp.py
+++ munp.py
@@ -2,10 +2,6 @@ import os
 
 import patoolib
 
-# 设置环境变量以支持GBK编码（适用于Linux和macOS）
-os.environ['LANG'] = 'zh_CN.GBK'
-os.environ['LC_ALL'] = 'zh_CN.GBK'
-
 
 def extract_archive(input_path: str, output_dir: str):
     # Ensure the destination directory exists
