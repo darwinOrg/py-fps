72a7a5332d3d93c21a66779a89b299dc902daf40
chenbiao <149027310@qq.com>
2025-01-07 18:02:21 +0800

    init

--- mdoc.py
+++ mdoc.py
@@ -4,6 +4,8 @@ import subprocess
 
 from weasyprint import HTML
 
+use_uno = os.getenv("USE_UNO") == 'true'
+
 
 def convert_file_format(input_path: str, output_path: str):
     if os.path.exists(output_path):
@@ -22,9 +24,14 @@ def convert_file_format(input_path: str, output_path: str):
         return
 
     output_format = output_ext[1:]
-    out_dir = os.path.dirname(output_path)
-    subprocess.run(['soffice', '--headless', '--convert-to', output_format, input_path, '--outdir', out_dir],
-                   check=True, capture_output=True, text=True, timeout=60)
-    converted_path = os.path.join(out_dir, pathlib.Path(input_path).stem + output_ext)
-    if converted_path != output_path:
-        os.rename(converted_path, output_path)
+
+    if use_uno:
+        subprocess.run(['unoconvert', '--convert-to', output_format, input_path, output_path],
+                       check=True, capture_output=True, text=True, timeout=60)
+    else:
+        out_dir = os.path.dirname(output_path)
+        subprocess.run(['soffice', '--headless', '--convert-to', output_format, input_path, '--outdir', out_dir],
+                       check=True, capture_output=True, text=True, timeout=60)
+        converted_path = os.path.join(out_dir, pathlib.Path(input_path).stem + output_ext)
+        if converted_path != output_path:
+            os.rename(converted_path, output_path)
