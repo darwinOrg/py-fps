28ebe38bcb55c89c9db56e467dd7c2e674579c41
chenbiao <149027310@qq.com>
2025-01-21 11:40:51 +0800

    init

--- mimg.py
+++ mimg.py
@@ -1,5 +1,6 @@
 import os
 
+import cv2
 import fitz
 from PIL import Image
 from loguru import logger
@@ -41,33 +42,46 @@ def pdf_to_image(pdf_path: str, output_dir: str, max_page: int, target_size: int
         current_height += img.size[1]
         img.close()
 
-    # 灰度化
-    with merged_image.convert('L') as gray_img:
-        merged_image.close()
-        gray_img.save(png_output_path, "PNG")
+    merged_image.save(png_output_path, "PNG")
+    merged_image.close()
+
+    # 使用OpenCV读取图像并转换为灰度
+    gray = cv2.imread(png_output_path, cv2.IMREAD_GRAYSCALE)
+
+    # 应用高斯模糊以减少噪声
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # 应用自适应阈值处理（这通常比简单的二值化更好）
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # 保存灰度化、二值化、高斯模糊后的图像
+    cv2.imwrite(png_output_path, thresh)
+
+    pic_size = os.path.getsize(png_output_path)
+    if pic_size <= target_size:
+        logger.info(f'pdf_to_image({pdf_path} -> {png_output_path}) success')
+        return png_output_path
+
+    out_img = Image.open(png_output_path)
+
+    for i in range(1, 19):
+        quality = (10 - i) * 10 if i < 10 else 19 - i
+        out_img.save(png_output_path, "PNG", quality=quality)
         pic_size = os.path.getsize(png_output_path)
         if pic_size <= target_size:
             logger.info(f'pdf_to_image({pdf_path} -> {png_output_path}) success')
             return png_output_path
 
-        for i in range(1, 19):
-            quality = (10 - i) * 10 if i < 10 else 19 - i
-            gray_img.save(png_output_path, "PNG", quality=quality)
-            pic_size = os.path.getsize(png_output_path)
-            if pic_size <= target_size:
-                logger.info(f'pdf_to_image({pdf_path} -> {png_output_path}) success')
-                return png_output_path
+    os.remove(png_output_path)
 
-        os.remove(png_output_path)
-
-        # 如果PNG失败，尝试保存为JPEG
-        for i in range(1, 19):
-            quality = (10 - i) * 10 if i < 10 else 19 - i
-            gray_img.save(jpeg_output_path, "JPEG", quality=quality)
-            pic_size = os.path.getsize(jpeg_output_path)
-            if pic_size <= target_size:
-                logger.info(f'pdf_to_image({pdf_path} -> {jpeg_output_path}) success')
-                return jpeg_output_path
+    # 如果PNG失败，尝试保存为JPEG
+    for i in range(1, 19):
+        quality = (10 - i) * 10 if i < 10 else 19 - i
+        out_img.save(jpeg_output_path, "JPEG", quality=quality)
+        pic_size = os.path.getsize(jpeg_output_path)
+        if pic_size <= target_size:
+            logger.info(f'pdf_to_image({pdf_path} -> {jpeg_output_path}) success')
+            return jpeg_output_path
 
     return ''
 
