c2953a387cb57e209668ca364c782883532a5dab
chenbiao <149027310@qq.com>
2025-01-23 10:24:36 +0800

    init

--- mdoc.py
+++ mdoc.py
@@ -12,22 +12,20 @@ def convert_file_format(input_path: str, output_path: str):
 
     _, output_ext = os.path.splitext(output_path)
 
-    if input_path.endswith('.html') and output_ext == '.pdf':
-        if platform.system() == "Linux":
-            subprocess.run(['xvfb-run', 'wkhtmltopdf', input_path, output_path],
-                           check=True, capture_output=True, text=True, timeout=60)
-        else:
-            subprocess.run(['wkhtmltopdf', input_path, output_path],
-                           check=True, capture_output=True, text=True, timeout=60)
-        return
-
-    if output_ext == '.md':
-        subprocess.run(['pandoc', '--toc', input_path, '-o', output_path],
+    if output_ext == '.md' or (input_path.endswith('.html') and output_ext == '.docx'):
+        subprocess.run(['pandoc', input_path, '-o', output_path],
                        check=True, capture_output=True, text=True, timeout=60)
         return
 
-    output_format = output_ext[1:]
+    if input_path.endswith('.html') and output_ext == '.pdf':
+        args = ['wkhtmltopdf', input_path, output_path]
+        if platform.system() == "Linux":
+            args.insert(0, 'xvfb-run')
 
+        subprocess.run(args, check=True, capture_output=True, text=True, timeout=60)
+        return
+
+    output_format = output_ext[1:]
     if use_uno:
         subprocess.run(['unoconvert', '--convert-to', output_format, input_path, output_path],
                        check=True, capture_output=True, text=True, timeout=60)
